0caa874336ef8b20483e35cfd43778e418ae14bd
rohitcodism <rohitpaula18i@gmail.com>
2023-07-20 21:42:52 +0530

    Hi

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 import win32com.client
 import speech_recognition as sr
-import os
+import webbrowser
+import openai
 
 speaker = win32com.client.Dispatch("SAPI.Spvoice")
 
+
 def takeCommand():
-    r = sr.Recogniser()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=0.4)
+        r.pause_threshold = 0.6
         audio = r.listen(source)
-        query = r.recoginze_google(audio, language="en-in")
-        print(f"User said: {query}")
-if __name__ == "__main__" :
-    s = takeCommand();
-    speaker.Speak(s)
+        try:
+            query = r.recognize_google(audio, language="en-in")
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Sorry can you repeat it again. I'm listening..."
+
+
+if __name__ == '__main__':
+    speaker.Speak("Hello I'm Jarvis AI")
+    speaker.Speak("Listening")
+    print("Listening...")
+    text = takeCommand()
+    while True:
+        sites = [["YouTube", "https://youtube.com"], ["Google", "https://google.com"],
+                 ["Linkedin", "https://linkedin.com"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in text.lower():
+                webbrowser.open(site[1])
+                speaker.Speak(f"Opening {site[0]}")
+        break
