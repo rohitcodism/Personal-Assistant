efb0aa3a5aa189c7ef4fa5469fd66a75be229b8d
rohitcodism <rohitpaula18i@gmail.com>
2023-07-30 11:57:13 +0530

    do it

--- main.py
+++ main.py
@@ -5,8 +5,48 @@ import openai
 import os
 import datetime
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import python_weather
+import spotipy
+import json
+import pywhatkit
+
+# OpenAI API #
+# API Key: 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
 
 speaker = win32com.client.Dispatch("SAPI.Spvoice")
+
+# Twilio API #
+#account_sid = ' AC4834f2764ff1b12135e6d433876fb6d7 '
+#auth_token = '200dfddce5f7a137e315ad292a8d579d'
+#userName = 'rohitpaulhhs04@gmail.com'
+
+# Spotify API #
+#client_id : b69c4958b8f24a4e9455062741a752a1
+#client_secret : 3c90bf6ce0704ac493807f7d843dad01
+
+# Spotify Music Functionality #
+userName = 'i8umlmo3e5brs22s6t87nsngk'
+clientID = 'b69c4958b8f24a4e9455062741a752a1'
+clientSecret = '3c90bf6ce0704ac493807f7d843dad01'
+redirect_uri = 'https://google.com'
+
+
+
+# Weather Function #
+async def getweather():
+    # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+    async with python_weather.Client(unit=python_weather.IMPERIAL) as client:
+        # fetch a weather forecast from a city
+        weather = await client.get(f'{text}')
+
+        # returns the current day's forecast temperature (int)
+        speaker.Speak(f"temperature is {weather.current.temperature} degree Farhenite")
+
+        # get the weather forecast for a few days
+        for forecast in weather.forecasts:
+            speaker.Speak(forecast)
+            break
 
 
 def takeCommand():
@@ -46,11 +86,24 @@ if __name__ == '__main__':
         if "the time" in text:
             strfTime = datetime.datetime.now().strftime("%H:%M:%S")
             speaker.Speak(f"Okay now its {strfTime}")
-        if "search" in text:
-            search_string = text
-            search_string = search_string.replace('','+')
-            browser = webdriver.Chrome()
-            for i in range(1):
-                browser.get('https://gooogle.com')
-                browser.find_element(search_string)
+        elif "Search" in text:
+            speaker.Speak("What do you want to search")
+            search_text = takeCommand().lower()
+            speaker.Speak(f"Searching {search_text}")
+            chrome_options = Options()
+            chrome_options.add_experimental_option("detach", True)
+            browser = webdriver.Chrome(options=chrome_options)
+            speaker.Speak(f"Opening {search_text}")
+            browser.get('https://google.co.in/search?q=' + search_text)
+        elif ("play song" or "Play Song") in text:
+           browser = webdriver.Chrome(Options().add_experimental_option("detach", True))
+           browser.fi
+        elif "I want to watch YouTube" in text:
+            speaker.Speak('Which youtube video do you want to watch ?')
+            mkv = takeCommand()
+            speaker.Speak(f"playing {mkv}")
+            print(f"playing {mkv}...")
+            pywhatkit.playonyt(mkv, open_video=True)
+        else:
+            exit(0)
         break
